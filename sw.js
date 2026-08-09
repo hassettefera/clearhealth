3d0a44be15df4cb8a2c101f6971c83955207d149
hassettefera <hasseteferaa@gmail.com>
2026-08-09 15:41:19 -0400

    Add service worker with caching functionality
    Implement service worker for caching assets.

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
+const CACHE_NAME = 'clearhealth-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/fuse.js@6.6.2'
+];
 
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
